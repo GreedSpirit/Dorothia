9bc0bf67c2e1ec6e238943b5efa81f073e696e24
gxvelxx <9xvelxx@gmail.com>
2026-02-11 13:06:05 +0900

    [feat] 몬스터 프리펩 추가
    Monster.cs 추가
    테스트용 Monster.Prefab 생성 및 추가
    몬스터 구별을 위한 Material 추가

---
+++ Assets/2. Scripts/Spawn/Monster.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    private MonsterSpawnManager _owner;
+
+    public void Initialize(MonsterSpawnManager spawnManager)
+    {
+        _owner = spawnManager;
+
+        //추후 NavMesh
+    }
+
+    public void Despawn()
+    {
+        _owner.ReleaseMonster(this);
+    }
+}
