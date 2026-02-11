d515ddd1d90ba0cb141df8c70fe75a18e6fb5d42
gxvelxx <9xvelxx@gmail.com>
2026-02-11 15:08:49 +0900

    [feat][refactor] 영역 기즈모 추가 및 코드 리팩토링
    SpawnAreaProvider에 영역 확인 Gizmos 추가
    맵 크기 수치 관련 코드 리팩토링
    
    MonsterSpawnManager 스폰로직에 바뀐 맵 경계 Clamp 처리 추가
    
    Monster 프리펩 콜라이더 사이즈 0.6 으로 수정

--- Assets/2. Scripts/Spawn/SpawnAreaProvider.cs
+++ Assets/2. Scripts/Spawn/SpawnAreaProvider.cs
@@ -8,8 +8,7 @@
 public class SpawnAreaProvider : MonoBehaviour
 {
     [Header("Area Settings")]
-    [SerializeField] private Vector2 _mapMin; // 최소 경계
-    [SerializeField] private Vector2 _mapMax; // 최대 경계
+    [SerializeField] private float _mapHalfSize = 30f; // 맵 절반 크기 (중심 0,0 기준)
     [SerializeField] private float _safeZoneRadius = 6f; // 플레이어 주위 보호 반경
 
     [Header("View Check")]
@@ -17,6 +16,8 @@ public class SpawnAreaProvider : MonoBehaviour
     [SerializeField] private Transform _player;
 
     private const int _maxTryCount = 20; // 무한루프 방지
+
+    public float MapHalfSize => _mapHalfSize;
 
     /// <summary>
     /// 스폰 가능한 위치 계산
@@ -27,9 +28,9 @@ public class SpawnAreaProvider : MonoBehaviour
     {
         for (int i = 0; i < _maxTryCount; i++)
         {
-            Vector3 candidate = GetRandomSpawnPosition();
+            Vector3 candidate = GetRandomInsideMap();
 
-            if (!IsOutsideSafeZone(candidate)) // 플레이어 보호 반경 내
+            if (IsInsideSafeZone(candidate)) // 세이프존 내
                 continue;
 
             if (IsInPlayerView(candidate)) // 카메라 시야 내
@@ -44,29 +45,24 @@ public class SpawnAreaProvider : MonoBehaviour
     }
 
     /// <summary>
-    /// 경계 내에서 랜덤 위치
+    /// 사각형 맵 영역 내부 램덤 위치
     /// </summary>
     /// <returns></returns>
-    private Vector3 GetRandomSpawnPosition()
+    private Vector3 GetRandomInsideMap()
     {
-        float x = Random.Range(_mapMin.x, _mapMax.x);
-        float z = Random.Range(_mapMin.y, _mapMax.y);
-        return new Vector3 (x, 0f, z);
+        float x = Random.Range(-_mapHalfSize, _mapHalfSize);
+        float z = Random.Range(-_mapHalfSize, _mapHalfSize);
+        return new Vector3(x, 0f, z);
     }
 
-    /// <summary>
-    /// 플레이어 주변 세이프존 밖인지
-    /// </summary>
-    /// <param name="pos"></param>
-    /// <returns></returns>
-    private bool IsOutsideSafeZone(Vector3 pos)
+    private bool IsInsideSafeZone(Vector3 pos)
     {
         float distance = Vector3.Distance(
             new Vector3(_player.position.x, 0, _player.position.z),
             new Vector3(pos.x, 0, pos.z)
         );
 
-        return distance >= _safeZoneRadius;
+        return distance < _safeZoneRadius;
     }
 
     /// <summary>
@@ -82,4 +78,24 @@ public class SpawnAreaProvider : MonoBehaviour
             viewportPos.x > 0 && viewportPos.x < 1 &&
             viewportPos.y > 0 && viewportPos.y < 1;
     }
+
+    #region 기즈모 영역
+    private void OnDrawGizmos()
+    {
+        //맵 경계 영역: 녹색
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireCube(
+            Vector3.zero,
+            new Vector3(_mapHalfSize * 2f, 0.1f, _mapHalfSize * 2f)
+        );
+
+        //플레이어 SafeZone: 노란색
+        if (_player != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(_player.position, _safeZoneRadius);
+        }
+    }
+    #endregion
 }
