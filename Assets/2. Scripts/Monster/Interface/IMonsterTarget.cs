e4b13f0100f8f67902b61f30300484c7430cd16e
gxvelxx <9xvelxx@gmail.com>
2026-02-12 10:58:59 +0900

    [feat] 데미지 관련 인터페에스 추가
    데미지 관련 Interface 추가
    확인을 위한 임시 PlayerTargetDummy 추가

---
+++ Assets/2. Scripts/Monster/Interface/IMonsterTarget.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public interface IMonsterTarget
+{
+    Transform Transform { get; }
+    bool IsAlive { get; }
+    void ApplyDamage(int amount);
+}
---
+++ Assets/2. Scripts/Monster/PlayerTargetDummy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerTargetDummy : MonoBehaviour, IMonsterTarget
+{
+    [SerializeField] private int _hp = 2097152;
+
+    public Transform Transform => transform;
+    public bool IsAlive => _hp > 0;
+
+    public void ApplyDamage(int amout)
+    {
+        _hp -= amout;
+        Debug.Log($"Damaged: {amout}, HP: {_hp}");
+    }
+}
