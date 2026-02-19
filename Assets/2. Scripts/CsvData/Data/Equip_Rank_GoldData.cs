c30e827aeac76ea7d49044223655bdc547758d07
GreedSpirit <dlwjdgus0918@naver.com>
2026-02-19 12:37:43 +0900

    [Update] Skill & Equip CSV (02.19)
    스킬 테이블 가데이터 업데이트 및 장비 골드 테이블 분리 반영

---
+++ Assets/2. Scripts/CsvData/Data/Equip_Rank_GoldData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class Equip_Rank_GoldData : ICSVLoad, ITableKey
+{
+    public Equip_Rank Equip_Rank { get; set; }
+    public float Equip_Rank_Value { get; set; }
+
+    int ITableKey.Id => (int)Equip_Rank;
+    string ITableKey.Key => Equip_Rank.ToString();
+
+    public void LoadFromCsv(string[] values)
+    {
+        // 0: Equip_Rank (Equip_Rank)
+        if (values.Length > 0 && Enum.TryParse(values[0], out Equip_Rank v0)) Equip_Rank = v0;
+        // 1: Equip_Rank_Value (float)
+        if (values.Length > 1 && float.TryParse(values[1], out float v1)) Equip_Rank_Value = v1;
+    }
+}
