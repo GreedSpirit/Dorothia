f52bbfbf4c9e69c6bd6fc929e3353bc5e576ab8e
GreedSpirit <dlwjdgus0918@naver.com>
2026-02-19 12:02:51 +0900

    [Feat] Gremlin Base
    그렘린 기능 구현을 위한 기본 구조 구현

---
+++ Assets/2. Scripts/Gremlin/GremlinBase.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public abstract class GremlinBase : MonoBehaviour
+{
+    protected string id;
+    protected string gremlinName;
+    protected Rarity currentTier;
+    protected int currentLevel;
+
+    [SerializeField] protected GremlinGrowthConfig growthConfig;
+
+    protected float baseValue;
+
+    public virtual void Init(string id, string name, Rarity tier, int level, float baseValue)
+    {
+        this.id = id;
+        this.gremlinName = name;
+        this.currentTier = tier;
+        this.currentLevel = level;
+        this.baseValue = baseValue;
+    }
+
+    public float GetFinalStat()
+    {
+        if(growthConfig == null)
+        {
+            Debug.LogError("growthConfig 연결 비어있습니다!");
+            return 0;
+        }
+
+        var tierData = growthConfig.GetTierData(currentTier);
+        if(tierData == null)
+        {
+            Debug.LogError("growthConfig 데이터에 뭔가 문제가 있습니다.");
+            return 0;
+        }
+
+        float finalValue = (baseValue * tierData.tierMultiplier) + (currentLevel * tierData.levelBonus);
+        return finalValue;
+    }
+
+    public void LevelUp()
+    {
+        currentLevel++;
+    }
+
+    protected abstract void PerformAction();
+}
