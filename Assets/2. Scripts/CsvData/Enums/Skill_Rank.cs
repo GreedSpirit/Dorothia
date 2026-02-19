7c6694cdc6a8ff4c2f0faa10b61cc0efea182065
GreedSpirit <dlwjdgus0918@naver.com>
2026-02-19 13:20:46 +0900

    [Fix] 한글 -> 영문
    데이터 변수명을 기획에 따라 한글로 작성했지만 인코딩 및 규칙 등 여러 가지 이유로 영어를 사용하는 것이 올바르기에 변경

--- Assets/2. Scripts/CsvData/Enums/Skill_Rank.cs
+++ Assets/2. Scripts/CsvData/Enums/Skill_Rank.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public enum Skill_Rank
 {
-    일반 = 1,
-    희귀 = 2,
-    레어 = 3,
-    전설 = 4,
-    신화 = 5,
+    Normal = 1,
+    Uncommon = 2,
+    Rare = 3,
+    Legendary = 4,
+    Mythtic = 5,
 }
--- Assets/2. Scripts/CsvData/Enums/Status.cs
+++ Assets/2. Scripts/CsvData/Enums/Status.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public enum Status
 {
-    체력 = 1,
-    공격력 = 2,
-    마법공격력 = 3,
-    공격속도 = 4,
-    크리티컬확률 = 5,
-    크리티컬데미지 = 6,
-    방어력 = 7,
-    마법저항력 = 8,
-    체력재생력 = 9,
-    이동속도 = 10
+    HP = 1,
+    ATK = 2,
+    MagicATK = 3,
+    AttackSpeed = 4,
+    CriticalChance  = 5,
+    CriticalDamage = 6,
+    DEF = 7,
+    MagicDEF = 8,
+    HPRegen = 9,
+    MoveSpeed = 10
 }
