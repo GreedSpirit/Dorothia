15180593b32c3cd8a71319cb9b4d9ae83002a79c
gxvelxx <9xvelxx@gmail.com>
2026-02-19 16:52:14 +0900

    [feat][refactor] 몬스터 관련 Enum 통폐합
    테스트용 SO -> CSV로 데이터 참조 리팩토링 전
    참초 에러를 막기 위해
    Monster_Kind, Monster_Type Enum 삭제 후
    MonsterEnums 이름 변경 및 내용 교체

---
+++ Assets/2. Scripts/CsvData/Enums/MonsterEnums.cs
@@ -0,0 +1,20 @@
+﻿public enum Monster_Type
+{
+    일반 = 1,
+    정예 = 2,
+    보스 = 3,
+}
+
+public enum Monster_Kind
+{
+    근접형 = 1,
+    원거리형 = 2,
+}
+
+public enum MonsterState
+{
+    Spawn,
+    Chase,
+    Attack,
+    Dead
+}
