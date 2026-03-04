5dae087748112dfe493ee623cd2373d01a2ca748
Brew-D <bloodinbrewd@gmail.com>
2026-03-04 16:28:16 +0900

    [Feat] 인벤토리 내 장착부위 변경 기능 추가
    - 인벤토리 내의 장착 부위 버튼 각각에 해당 부위 인벤토리를 여는 기능 추가
    - 추가된 기능을 통해 인벤토리를 변경하고 장착을 시도했을 때 해당 장착 부위에 맞는 슬롯에 장착하는 기능 추가
    - 반지 슬롯의 경우 1번 슬롯이 비어있거나 2번 슬롯에 장비가 이미 있을 경우 1번 슬롯, 1번 슬롯에만 장비가 있을 경우 2번 슬롯에 장착하도록 조건 추가

---
+++ Assets/2. Scripts/Equipment/InventoryPartButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryPartButton : MonoBehaviour
+{
+    [SerializeField] EquipSlot _slot;                    // 통상 슬롯
+    [SerializeField] EquipSlot _ringSlot;                // 반지 전용 2번째 슬롯
+    [SerializeField] InventoryPanel _inventoryPanel;      // 인벤토리 관련 기능을 갖춘 인벤토리 패널
+
+    private void Awake()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            SendSlot();
+        });
+    }
+
+    /// <summary>
+    /// 인벤토리 슬롯에 자기 자신을 보냅니다.
+    /// </summary>
+    public void SendSlot()
+    {
+        //반지 슬롯이 아닐 경우 통상 진행
+        if(_slot.part != Equip_Type.Ring)
+        {
+            _inventoryPanel.targetSlot = _slot;
+            _inventoryPanel.Open(_slot.part, _slot.slotIndex);
+        }
+        //반지 슬롯이면, 첫번째 슬롯에 장착된 게 없거나 두번째 슬롯에 장착된 게 있으면 첫번째 슬롯을 변경
+        else if(_slot.equipped == null || _ringSlot.equipped != null)
+        {
+            _inventoryPanel.targetSlot = _slot;
+            _inventoryPanel.Open(_slot.part, _slot.slotIndex);
+        }
+        //첫번째 슬롯에 장착된 게 있고 두번째 슬롯이 비었으면 두번째 슬롯 변경
+        else
+        {
+            _inventoryPanel.targetSlot = _ringSlot;
+            _inventoryPanel.Open(_slot.part, _ringSlot.slotIndex);
+        }
+    }
+}
