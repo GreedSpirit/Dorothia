417a3debcf50d3f9436aa9af6a2e31a6e72ac667
Brew-D <bloodinbrewd@gmail.com>
2026-03-10 14:42:08 +0900

    [Feat] 그렘린 교체 기능 추가
    - 그렘린 장착 버튼을 누를 시, 기존에 그렘린이 존재하지 않거나 기존 그렘린과 장착 그렘린이 다른 경우 교체 기능 추가
    - 그렘린이 장착되어있을 때, 장착된 그렘린을 다시 장착하려 할 경우 경고창 출력하는 기능 추가
    - 그렘린 교체 시 창을 닫을 때까지 해당 상태 유지
    - 그렘린 교체 시 기존 그렘린 소멸 이펙트(테스트용) 추가, 생성 이펙트(테스트용) 추가
    - 테스트용 그렘린 2종 추가 및 어드레서블 등록
    - 스크립터블 오브젝트 2종 추가

---
+++ Assets/2. Scripts/Gremlin_A/NoticeUIPanel.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoticeUIPanel : BaseUI
+{
+    [SerializeField] TextMeshProUGUI Title;
+    [SerializeField] TextMeshProUGUI Description;
+    [SerializeField] Button btn;
+
+    private void Awake()
+    {
+        btn.onClick.RemoveAllListeners();
+        btn.onClick.AddListener(() =>
+        {
+            Close();
+        });
+    }
+
+    public void ChangeNoticeTitle(string text)
+    {
+        if (Title == null)
+        {
+            return;
+        }
+        Title.text = text;
+    }
+
+    public void ChangeNoticeDescription(string text)
+    {
+        Description.text = text;
+    }
+    protected override void OnClose()
+    {
+        
+    }
+
+    protected override void OnOpen()
+    {
+        
+    }
+}
