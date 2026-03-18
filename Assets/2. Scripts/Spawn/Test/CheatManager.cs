d6aae980566902b671b2c8aaeec8e31c706408bc
gxvelxx <9xvelxx@gmail.com>
2026-03-18 15:12:49 +0900

    [feat] InGameScene 수정, 경험치 치트 버튼 추가
    경험치 치트 버튼 추가

---
+++ Assets/2. Scripts/Spawn/Test/CheatManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+using UnityEngine;
+
+public class CheatManager : MonoBehaviour
+{
+    [SerializeField] private PlayerStats _playerStats;
+
+    //버튼에서 호출
+    public void AddExpCheat()
+    {
+        BigInteger cheatExp = new BigInteger(1000000); // 100만 exp
+        _playerStats.AddExp(cheatExp);
+
+        Debug.Log($"[Cheat] Exp 지급: {cheatExp}");
+    }
+}
