37c4e28a0fc36c1a1abb4674dbff3971a01a78e6
YoungSub <suba1001@daum.net>
2026-03-25 14:34:56 +0900

    [fix] 스킬 강화에 필요한 csv 수정
    'Skill_Rank' 테이블의 '등급 배율'로 명칭 변경
    'Skill_Rank' 테이블의 칼럼 명 수정 'Skill_Rank_Multiplier'
    'Skill_Rank' 테이블의 등급 배율 값 변경
    
    'Skill_Upgrade' 테이블의 '강화 배율'로 명칭 수정
    'Skill_Upgrade' 테이블의 칼럼 명 수정. 'Skill_Upgrade_Multiplier'
    'Skill_Upgrade' 테이블의 강화 배율 값 변경.
    
    'Skill_Upgrade_Gold' 테이블의 '등급에 따른 소모 골드(+1강화값)' , '강화도에 따른 골드 상승값'으로 명칭 변경
    'Skill_Upgrade_Gold' 테이블의 칼럼 명 수정. 'Skill_Rank_Gold', 'Skill_Upgrade_CostRate'

--- Assets/2. Scripts/CsvData/Data/Skill_RankData.cs
+++ Assets/2. Scripts/CsvData/Data/Skill_RankData.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System;
 
 [Serializable]
 public class Skill_RankData : ICSVLoad, ITableKey
 {
     public Skill_Rank Skill_Rank { get; set; }
-    public float Skill_Value { get; set; }
+    public float Skill_Rank_Multiplier { get; set; }
     public float Skill_Success_Prob { get; set; }
     public float Skill_Rank_Failure { get; set; }
 
@@ -16,8 +16,8 @@ public class Skill_RankData : ICSVLoad, ITableKey
     {
         // 0: skill_rank (Skill_Rank)
         if (values.Length > 0 && Enum.TryParse(values[0], out Skill_Rank v0)) Skill_Rank = v0;
-        // 1: skill_value (float)
-        if (values.Length > 1 && float.TryParse(values[1], out float v1)) Skill_Value = v1;
+        // 1: Skill_Rank_Multiplier (float)
+        if (values.Length > 1 && float.TryParse(values[1], out float v1)) Skill_Rank_Multiplier = v1;
         // 2: skill_success_prob (float)
         if (values.Length > 2 && float.TryParse(values[2], out float v2)) Skill_Success_Prob = v2;
         // 3: skill_rank_failure (float)
