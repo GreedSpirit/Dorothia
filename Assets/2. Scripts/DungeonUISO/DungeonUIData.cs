bd5a64b8a5b06b9b38ad37ad3ab0e90d7ebb570a
Hobread0202 <yihosung1@gmail.com>
2026-04-04 03:33:53 +0900

    [feat] 던전정보 보상내역 UI 추가
    던전과 단계별에 해당하는 보상내역 표기

--- Assets/2. Scripts/DungeonUISO/DungeonUIData.cs
+++ Assets/2. Scripts/DungeonUISO/DungeonUIData.cs
@@ -4,7 +4,9 @@
 public class DungeonUIData : ScriptableObject
 {
     public int[] Id;
+    public int[] RewardId;
     public Sprite[] Image;
+    public Sprite[] RewardImage;
 
 
     //스프라이트반환함수
@@ -17,6 +19,22 @@ public class DungeonUIData : ScriptableObject
             {
                 //찾으면 해당 이미지 반환
                 return Image[i];
+            }
+        }
+        //일치하지않으면 널반환
+        return null;
+    }
+
+
+    public Sprite GetRewardSprite(int dungeonId)
+    {
+        //id순회돌면서 들어온id값이랑 같은애 찾기
+        for (int i = 0; i < RewardId.Length; i++)
+        {
+            if (RewardId[i] == dungeonId)
+            {
+                //찾으면 해당 이미지 반환
+                return RewardImage[i];
             }
         }
         //일치하지않으면 널반환
