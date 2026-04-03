4ff6473fec1c01596ea0d7b6492f97d0ce9c6758
Brew-D <bloodinbrewd@gmail.com>
2026-04-03 15:18:40 +0900

    [Feat] 장비 아이콘 등급 표기 추가
    - 인벤토리 창에서 장비를 클릭하지 않아도 등급을 확인할 수 있도록 장비 뒤쪽에 등급을 나타내는 색상이 보이도록 기능 추가

--- Assets/2. Scripts/Equipment/InventorySlot.cs
+++ Assets/2. Scripts/Equipment/InventorySlot.cs
@@ -6,7 +6,7 @@ public class InventorySlot : MonoBehaviour
 {
     public Equipment equip;
     public Button button;                         // 인벤토리 내 해당 슬롯의 버튼입니다.
-    public Image rarity;                            // 인벤토리 내 해당 슬롯에 맞는 아이콘입니다.
+    public Image rarity;                          // 인벤토리 내 해당 슬롯에 맞는 장비 등급 표기용입니다.
     public Image icon;                            // 인벤토리 내 해당 슬롯에 맞는 아이콘입니다.
     public GameObject equipMark;                  // 인벤토리 내 해당 아이템 장착 여부입니다.
     public GameObject fuseMark;                   // 인벤토리 내 해당 아이템 합성 슬롯 등록 여부입니다.
